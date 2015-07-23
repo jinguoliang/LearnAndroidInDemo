708d1840a6e96a05d46a1801acec8ad3a5cfcf5b
jinguoliang <jinguol999@163.com>
2015-07-23 16:59:39 +0800

    [Sample] Android L的动画, 转场动画

--- app/src/main/java/com/example/jinux/mydemo/materail_animation/TestMaterialTransition.java
+++ app/src/main/java/com/example/jinux/mydemo/materail_animation/TestMaterialTransition.java
@@ -2,9 +2,15 @@ package com.example.jinux.mydemo.materail_animation;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.jinux.mydemo.R;
 
@@ -17,5 +23,17 @@ public class TestMaterialTransition extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_tranisition);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public void onImageClick(View view) {
+        ImageView iv = (ImageView) view;
+
+        AnimatedVectorDrawable d = (AnimatedVectorDrawable) getDrawable(R.drawable.animvectordrawable);
+        iv.setImageDrawable(d);
+        d.start();
+
+
+
     }
 }
